fed5d93f821384ba293d681f200595461247c799
Unknown <willi11-95@hotmail.com>
2018-03-11 19:01:10 -0500

    Uso de templates o plantillas en Django

--- workspace/geographic/geographic/countries/views.py
+++ workspace/geographic/geographic/countries/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 #renderizamos con el render
 from django.http import HttpResponse
 #importamos las vistas
-from django.views.generic import View
+from django.views.generic import TemplateView
 # Create your views here.
 # definimos una funcion para que de devuelva una respuesta HTTP
-class HomeView(View):
-	def get(self, request, *args, **kwargs):
-		return render(request, "countries/home.html")
+class HomeView(TemplateView):
+	template_name = 'countries/home.html'
+	#forma de pasar inforacion a traves de los templatesviews
+	def get_context_data(self, *args, **kwargs):
+		colombia = {'name': 'colombia', 'code':'CO'}
+		usa = {'name': 'estados unidos', 'code':'USA'}
+		mexico = {'name': 'mexico', 'code': 'MX'}
+		countries = [colombia, usa, mexico]
+		return {'countries': countries}
 
