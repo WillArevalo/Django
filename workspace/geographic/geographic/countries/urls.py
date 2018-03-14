ca3462868b78196428c8e5b16eea84685dc7fd4e
Unknown <willi11-95@hotmail.com>
2018-03-14 10:59:24 -0500

    include

--- workspace/geographic/geographic/countries/urls.py
+++ workspace/geographic/geographic/countries/urls.py
@@ -13,26 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 #importo include de django.urls
 from django.urls import path, include
 #Importamos home
 from countries.views import (
-    HomeView, 
-    TagsView, 
     CountryDetailView,
     CountryDetailIdView
     )
-from continents.views import ContinentsView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     #En las urls de la app principal le diremos que procese nuestra vista de la mini-app
     # Si utilizo name puede hacer referencia de los templates
-    path('', HomeView.as_view(), name="home"),
-    path('tags/', TagsView.as_view(), name="tags"),
-    #paso en el include el archivo donde se encuentran almacenadas las urls de mi app
-    #Este namespace funciona solo al nivel interno no se refelja en la url, sirve para identificar espacio de trabajo
-    path('continents/', include("continents.urls", namespace="continents")),
-    path('countries/', include("countries.urls")),
+    #Valido que es un entero
+    path('<int:id>/', CountryDetailIdView.as_view(), name="country_id_detail"),
+    path('<code>/', CountryDetailView.as_view(), name="country_detail"),
 ]
