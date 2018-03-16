367721d1a898030b4b76b3cd0ea5adbda3b15ea2
Unknown <willi11-95@hotmail.com>
2018-03-15 22:08:19 -0500

    Creando el primer modelo en Django

--- workspace/geographic/geographic/countries/context_processor.py
+++ workspace/geographic/geographic/countries/context_processor.py
@@ -1,19 +1,9 @@
+from countries.models import Country
+#importamos nuestro modelo Country
 from django.urls import reverse
 #importamos el metodo para hacer reverse en las urls path
 def countries_data(request):
 	#Se pasa el nombre del path para crear el datail_url
-	colombia = {
-	'name': 'colombia', 'code':'CO', 'id': 1,
-	'detail_url': reverse("country_id_detail", kwargs={'id':1})
-	}
-	usa = {
-	'name': 'estados unidos', 'code':'USA', 'id': 2,
-	'detail_url': reverse("country_id_detail", kwargs={'id':2})
-	}
-	mexico = {
-	'name': 'mexico', 'code': 'MX', 'id': 3,
-	'detail_url': reverse("country_id_detail", kwargs={'id':3})
-	}
-	countries = [colombia, usa, mexico]
+	countries = Country.objects.all()
 	population = 10
 	return {'countries': countries, 'population': population}
--- workspace/geographic/geographic/countries/models.py
+++ workspace/geographic/geographic/countries/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# Este modelo representara un pais dentro de nuestra base de datos
+class Country(models.Model):
+	CODES_CHOICES = (
+		('colombia', 'CO'),
+		('mexico', 'MX'),
+		('estados unidos', 'USA'),
+		('argentina', 'AR'),
+	)
+	#Los modelos internamente tienen un id autoincremental
+	#Campo de tipo char que necesita el maximo de longitud de la cadena
+	name = models.CharField(max_length=255)
+	#Si queremos limitar a una cierta tipa de opcione que utilice los codigos de arriba
+	code = models.CharField(max_length=3, choices=CODES_CHOICES)
+
+	
