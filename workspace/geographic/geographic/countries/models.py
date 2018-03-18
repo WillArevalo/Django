76cd5c06a6a9522e1bd84e698716b3db7f8994a3
Unknown <willi11-95@hotmail.com>
2018-03-17 23:29:12 -0500

    Creación de un Custom Manager, o Administrador Personalizado
    Shell en el prox commit

--- workspace/geographic/geographic/countries/models.py
+++ workspace/geographic/geographic/countries/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+#Creamos un Custom Manager
+class ActiveManager(models.Manager):
+	def get_queryset(self):
+		return super().get_queryset().filter(active=True)
+
 # Este modelo representara un pais dentro de nuestra base de datos
 class Country(models.Model):
+
 	CODES_CHOICES = (
 		('colombia', 'CO'),
 		('mexico', 'MX'),
@@ -17,6 +24,12 @@ class Country(models.Model):
 	#Genero un nuevo campo para hacer la relacione entre continents y countries
 	continent = models.ForeignKey('continents.Continent', on_delete=models.CASCADE)
 	#Borrado en modo casacada para que cuando se borre un continent se borren todos los paises asociados
+	active = models.BooleanField(default=True)
+	#Active es un campo que si sirve para mostrar o no mostrar un campo
+	active_manager = ActiveManager()
+	#active_manager para filtrar y solo mostrar los que debemos
+	objects = models.Manager()
+	#Nuestro objects por defecto desaparece asi que los volvemos a instanciar
 
 	#funcion para mostrar el nombre del objeto en vez de un nombre generico
 	def __str__(self):
