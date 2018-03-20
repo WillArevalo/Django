a71f8f11004c83dee96c18668d8ba80b13a8f507
Unknown <willi11-95@hotmail.com>
2018-03-19 22:07:46 -0500

    Tipos de datos

--- workspace/geographic/geographic/people/forms.py
+++ workspace/geographic/geographic/people/forms.py
@@ -1,5 +1,14 @@
 from django import forms
+from countries.models import Country
+from people.models import Person
 
 class RegisterForm(forms.Form):
 	first_name = forms.CharField(label='first name')
-	email = forms.EmailField(label='email')
+	#email = forms.EmailField(label='email')
+	#date = forms.DateField(label='date')
+	#is_active = forms.BooleanField(label='active')
+	#campos con modelos
+	#Many to Many
+	nacionality = forms.ModelMultipleChoiceField(queryset=Country.objects.all())
+	#One to Many
+	father = forms.ModelChoiceField(queryset=Person.objects.all())
--- workspace/geographic/geographic/people/models.py
+++ workspace/geographic/geographic/people/models.py
@@ -8,3 +8,7 @@ class Person(models.Model):
 	#relacion uno a uno con el padre de la persona que se llama a si misma con self
 	father = models.OneToOneField('self',on_delete=models.CASCADE,null=True)
 	#Por lo tanto con el null=TRUE si una persona se crea no va a ser obligatorio que rellene el campo father
+
+	#funcion para mostrar el nombre del objeto en vez de un nombre generico
+	def __str__(self):
+		return self.first_name
